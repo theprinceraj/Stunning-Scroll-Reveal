6d9f1c98a2ad2a9e8ef47ae77a238b5996a9e5b8
theprinceraj <discordsimpcity+github@gmail.com>
2023-07-19 15:57:09 +0530

    .

--- main.js
+++ main.js
@@ -8,10 +8,23 @@ let tl = gsap.timeline({
     }
 });
 
-tl.to(".images-container>.image-card>img", { scale: 1 }, 0);
-tl.to(".images-container", { scale: 2, rotate: 0 }, 0);
+tl.to(".images-container>.image-card>img", {
+    scale: 1
+}, 0);
+
+tl.to(".images-container", {
+    scale: 2,
+    rotate: 0
+}, 0);
 
 
-tl.to(".text-layer", { height: "100%" }, 0.3);
-tl.to(".text-layer h1", { scale: 1 }, 0.8);
-tl.to(".images-container", { scale: 0.8, opacity: 0.2 }, 0.7);
+tl.to(".text-layer", {
+    height: "100%"
+}, 0.3);
+tl.to(".text-layer h1", {
+    scale: 1
+}, 0.8);
+tl.to(".images-container", {
+    scale: 0.8,
+    opacity: 0.2
+}, 0.7);
