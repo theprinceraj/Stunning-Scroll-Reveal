6d2168ecf82feaa00c6e0ed34f6b9ea1428f5021
theprinceraj <discordsimpcity+github@gmail.com>
2023-07-19 17:13:45 +0530

    .

--- main.js
+++ main.js
@@ -21,9 +21,11 @@ tl.to(".images-container", {
 tl.to(".text-layer", {
     height: "100%"
 }, 0.3);
+
 tl.to(".text-layer h1", {
     scale: 1
 }, 0.8);
+
 tl.to(".images-container", {
     scale: 0.8,
     opacity: 0.2
